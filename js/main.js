ee6eaa17c474a7f0cb464aaf5ea0d560e3c0a6a8
Liam Ellis <liamhellis@gmail.com>
2017-01-30 13:16:21 -0500

    carousel, on scroll nav

--- js/main.js
+++ js/main.js
@@ -1,21 +1,9 @@
-function initMap(){
-    var where = {lat: 40.730610, lng: -73.935242};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: where,
-        zoom: 15,
-        scrollwheel: false,
-        styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
-    });
-    var marker = new google.maps.Marker({
-        position: where,
-        map: map
-    });
-}
+// Deleted Google Map Code
 
 retinajs();
 
 $("#slider-animation").typed({
-    strings: ["HELLO!", "CZEŚĆ!", "Ahoj!", "你好!", "Hallo!", "Salut!", "Hola!", "Ciao!", "Oi!", "Godan!", "Здравствуй!", "Chào!"],
+    strings: ["Hey", "Hi", "Heyy", "Hi tho", "Sup"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 2000,
@@ -30,14 +18,19 @@ $("#slider-animation-2").typed({
 });
 
 $(window).scroll(function(){
-    if( $(window).scrollTop() > 70){
+    if( $(window).scrollTop() > 225){
         $('.header_dynamic').removeClass('header_white').css({
-            'background-color': '#ffffff'
+            'background-color': '#ffffff',
+            'color': '#333',
+            'opacity': .7
         });
+        $('.logo').fadeIn(500);
+        $('.header_dynamic').addClass('fixed');
     }else{
-        $('.header_dynamic').addClass('header_white').css({
+        $('.header_dynamic').addClass('header_white').removeClass('fixed').css({
             'background-color': ''
-        })
+        });
+        $('.logo').fadeOut(500);
     }
 });
 
@@ -70,6 +63,18 @@ $(document).ready(function(){
             enabled: true
         },
         mainClass: 'mfp-fade'
+    });
+
+    $(".post-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText: ['', ''],
+        responsive:{
+            0:{
+                items:1
+            }
+        }
     });
 
     $(".opinions").owlCarousel({
